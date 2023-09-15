3319bae1fbd0a303796e708f7c3de848aef67294
marshpinvralt <marshpinvralt@gmail.com>
2023-09-15 11:32:10 -0400

    simplified main.rs

--- README.md
+++ README.md
@@ -9,10 +9,8 @@ A very simple and lightweight AUR Helper written in Rust.
 ## Features
 
 - Sync packages
-- Remove packages
-
 
 
 ## Notice
 
-This project is a __HEAVY__ work in progress. The only working feature is a very basic sync command, which may still be error-prone. __Do NOT use this if you want a functioning AUR Helper.__
+This project is a __NOT SO HEAVY__ work in progress. The only working feature is a very basic sync command, which may still be error-prone. __MAYBE use this if you want a functioning AUR Helper.__
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,34 @@
-extern crate clap;
-
 pub mod pkg;
 pub mod git;
 
 use std::{
     env,
-    collections::HashMap
+    process
 };
-
-//use clap::{Command, ArgGroup, arg};
-//use pkg::{sync, remove, get_source};
 
 fn get_args() -> Vec<String> {
     env::args().collect()
 }
 
-fn collect_args(raw_args: Vec<String>) -> HashMap<String, String> {
-    let mut args: HashMap<String, String> = HashMap::new();
-    // OPERATION : FIRST KEY //
-    args.insert(raw_args[1], String::from("None"));
-
-    args
+fn process_args(raw_args: Vec<String>) {
+    if raw_args[1] != String::from("-S") && raw_args[1] != String::from("-h") && raw_args[1] != String::from("--sync") && raw_args[1] != String::from("--help") {
+        println!(r#"Invalid argument! Please enter "raur -h" for help."#);
+        process::exit(1);
+    } else if raw_args[1] == String::from("-S") || raw_args[1] == String::from("--sync") && raw_args.len() == 3 {
+        pkg::sync(raw_args[2].to_owned());
+    } else if raw_args[1] == String::from("-h") || raw_args[1] == String::from("--help") && raw_args.len() == 2 {
+        println!(r#"usage: raur <OPERATION> <ARGUMENT>
+  options:
+    -S, --sync <PACKAGE> Syncs AUR package to system.
+    -h, --help           Shows help text.
+    "#);
+    } else {    
+        println!(r#"Not enough or too many arguments! Please enter "raur -h" for help."#);
+        process::exit(1);
+    }
 }
 
 fn main() {
-    /*let raur = Command::new("raur")
-        .args(&[
-            // OPERATIONS //
-            arg!(-S --sync <PACKAGE> "Syncs AUR repository to system."),
-            arg!(-R --remove <PACKAGE> "Removes synced package from system."),
-            arg!(-T --test <TEST> "Test command"),
-            // OPERATION FLAGS //
-            arg!(-n --noconfirm "Skips pacman [y/N] confirmation.")
-        ])
-        .groups(&[
-            ArgGroup::multiple(ArgGroup::new("sync_args").args(["sync", "noconfirm"]), true),
-            ArgGroup::multiple(ArgGroup::new("remove_args").args(["remove", "noconfirm"]), true),
-            ArgGroup::multiple(ArgGroup::new("test_args").args(["test"]), true)
-        ])
-    .get_matches();
-
-
-
-    if let Some(pkg_name) = raur.get_one::<String>("sync") {
-        sync(pkg_name.to_string());
-    } else if let Some(pkg_name) = raur.get_one::<String>("remove") {
-        remove(pkg_name.to_string());
-    } else if let Some(_pkg_name) = raur.get_one::<String>("test") {
-        println!("{}", get_source(&String::from("PKGBUILD")));
-    }*/
-
     let raw_args = get_args();
-
-    dbg!(collect_args(raw_args));
+    process_args(raw_args);
 }
